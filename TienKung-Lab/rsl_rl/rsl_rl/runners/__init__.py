0c8dd42fcd1c059c46a688404f957a4015067826
Yufei Liu <1320264328@qq.com>
2026-01-21 09:40:45 +0800

    fix runner bug

--- TienKung-Lab/rsl_rl/rsl_rl/runners/__init__.py
+++ TienKung-Lab/rsl_rl/rsl_rl/runners/__init__.py
@@ -22,4 +22,4 @@ from .amp_on_policy_runner import AmpOnPolicyRunner
 from .on_policy_runner import OnPolicyRunner
 from .dwaq_on_policy_runner import DWAQOnPolicyRunner
 
-__all__ = ["OnPolicyRunner", "AmpOnPolicyRunner", "AMPOnPolicyRunnerMulti"]
+__all__ = ["OnPolicyRunner", "AmpOnPolicyRunner", "DWAQOnPolicyRunner"]
